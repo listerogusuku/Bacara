4ffb2a1be1b623e592a47ed135dc2ced140001b2
listerogusuku <72938287+listerogusuku@users.noreply.github.com>
2020-10-18 18:57:48 -0300

    Inicio_do_codigo.py
    Implementação do início do código.

--- bacara1.py
+++ bacara1.py
@@ -1,3 +1,21 @@
-print('teste inicial')
-print('oi')
-print('Oi, Lister! rs')
+# EP | Exercício Programa: Bacará Simplificado | Design de Software
+# Equipe: Celina Vieira de Melo e Lister Ogusuku Ribeiro
+# Data: 18/10/2020
+
+#Descrição:
+#Jogo de bacará criado com a intenção de promover uma interatividade com o usuário,
+#onde este aposta em quem será o vencedor da partida (jogador, banco ou empate) e, por fim, todo
+#o restante do jogo é realizado pela mesa de acordo com as regras simplificadas definidas ao longo
+#do programa pelo programador.
+
+#Neste jogo foram implementadas todas as regras avançadas.
+
+#============================
+
+#Importação da biblioteca "Random", que ajudará meu código a gerar números aleatórios
+
+import random
+
+#Iniciando o jogo e entregando a quantidade n de fichas definida pelo programador.
+
+print("Começo do jogo \nVocê recebeu 100 fichas! ") #Programa diz ao jogador que o jogo começou e que ele recebeu 100 fichas.

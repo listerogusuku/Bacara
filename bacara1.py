0ef626fb2fe2e8db672b1fc6d1401594dbac5788
CelinaVMelo <72881115+CelinaVMelo@users.noreply.github.com>
2020-10-18 22:31:21 -0300

    Update bacara1.py

--- bacara1.py
+++ bacara1.py
@@ -19,3 +19,13 @@ import random
 #Iniciando o jogo e entregando a quantidade n de fichas definida pelo programador.
 
 print("Começo do jogo \nVocê recebeu 100 fichas! ") #Programa diz ao jogador que o jogo começou e que ele recebeu 100 fichas.
+soma=100
+aux=0
+while aux==0:
+    while soma>0:
+        Aposta=input("Em quem você quer apostar? ")               # "Aposta" recebe a aposta do usuário do jogo.
+        while Aposta != 'Jogador' and Aposta!='jogador' and Aposta !='Banco' and Aposta != 'banco' and Aposta!='Empate' and Aposta!='empate':
+            print("Não entendi, em quem você quer apostar? ")     # "Fichas" recebe o valor de fichas que o apostador deseja apostar.
+            Aposta=input("Em quem você quer apostar? ")
+
+        Fichas=int(input("Quantas fichas você quer apostar? "))

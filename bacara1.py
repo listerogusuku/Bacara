124edb26031c3972d6b5043bc07a046b0a16a36d
listerogusuku <72938287+listerogusuku@users.noreply.github.com>
2020-10-18 22:39:45 -0300

    sorteios.py
    Definição das cartas 1 e 2 do jogador e suas somas.

--- bacara1.py
+++ bacara1.py
@@ -54,3 +54,47 @@ while aux==0:
             i += 1
         print('Seu baralho possui', numero_de_baralhos*(len(baralho)-1)*4, 'cartas') 
 
+
+        # Sorteia uma carta e um naipe por meio da biblioteca random:
+
+        carta1_Jogador=random.randint(1,13)
+        naipe1_Jogador=random.randint(0,3)
+        c1j=baralho[carta1_Jogador]
+        n1j=naipe[naipe1_Jogador]
+
+        # While que atribui às cartas maiores que 9 o valor 0 e mostra ao usuário qual carta ele tirou:
+
+        while num_cartas[carta1_Jogador][naipe1_Jogador]==0:
+            carta1_Jogador=random.randint(1,13)
+            naipe1_Jogador=random.randint(0,3)
+            c1j=baralho[carta1_Jogador]
+            n1j=naipe[naipe1_Jogador]
+        num_cartas[carta1_Jogador][naipe1_Jogador]-=1
+
+        if baralho[carta1_Jogador]==10 or baralho[carta1_Jogador]=='K' or baralho[carta1_Jogador]=='Q' or baralho[carta1_Jogador]=='J':
+            carta1_Jogador=0
+        print("A primeira carta do jogador é {} de {}, que equivale a {} ".format(c1j,n1j,carta1_Jogador))
+
+        # Sorteia uma carta e um naipe por meio da biblioteca random:
+
+        carta2_Jogador=random.randint(1,13)
+        naipe2_Jogador=random.randint(0,3)
+        c2j=baralho[carta2_Jogador]
+        n2j=naipe[naipe1_Jogador]
+
+        # While que atribui às cartas maiores que 9 o valor 0 e mostra ao usuário qual carta ele tirou:
+
+        while num_cartas[carta2_Jogador][naipe2_Jogador]==0:
+            carta2_Jogador=random.randint(1,13)
+            naipe2_Jogador=random.randint(0,3)
+            c2j=baralho[carta2_Jogador]
+            n2j=naipe[naipe2_Jogador]
+        num_cartas[carta2_Jogador][naipe2_Jogador]-=1
+        if baralho[carta2_Jogador]==10 or baralho[carta2_Jogador]=='K' or baralho[carta2_Jogador]=='Q' or baralho[carta2_Jogador]=='J':
+            carta2_Jogador=0
+        print("A segunda carta do jogador é {} de {}, que equivale a {}".format(c2j,n2j,carta2_Jogador))
+
+        # Mostra a soma das cartas do jogador:
+
+        soma_jogador=(carta1_Jogador+carta2_Jogador)%10
+        print('A soma das cartas do jogador é {} '.format(soma_jogador))
